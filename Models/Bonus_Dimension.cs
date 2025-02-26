ad6d9da7775072c172fb84f51518584eb8f96a05
Jim Burlison <JBurlison@Gmail.com>
2025-02-25 21:11:45 -0500

    Add project files.

---
+++ Models/Bonus_Dimension.cs
@@ -0,0 +1,39 @@
+﻿namespace ProjectMMOConfigurator.Models
+{
+    public class Dimension
+    {
+        public Dictionary<string, int> travel_req { get; set; } = [];
+        public required Global_Mob_Modifier[] global_mob_modifier { get; set; }
+
+        /// <summary>
+        /// Dictionary of mob modifiers for each mob type
+        /// Key is the mob type, value is a dictionary of attribute modifiers
+        /// <code>
+        ///           "minecraft:zombie": {
+        ///            "minecraft:generic.max_health": 0.5, //half health
+        ///            "minecraft:generic.movement_speed": 2.0, //double speed
+        ///            "minecraft:generic.attack_damage": 1.1 // 10% increase in damage
+        ///         },
+        ///          "minecraft:skeleton": {
+        ///            "minecraft:generic.attack_damage": 1.15  //not all attributes need to have values, only what you want to modify.
+        ///          }
+        /// </code>
+        /// </summary>
+        public Dictionary<string, Dictionary<string, float>> mob_modifier { get; set; } = [];
+        public required string[] vein_blacklist { get; set; }
+        public bool Override { get; set; }
+        public required Bonus bonus { get; set; }
+    }
+
+    public class Bonus
+    {
+        public Dictionary<string, float> Dimension { get; set; } = [];
+    }
+
+    public class Global_Mob_Modifier
+    {
+        public required string attribute { get; set; }
+        public float amount { get; set; }
+        public required string operation { get; set; }
+    }
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectMMOConfigurator.Models
+{
+    public class Skill
+    {
+        public int maxLevel { get; set; } = 2147483647;
+        public required string icon { get; set; }
+        public int iconSize { get; set; } = 32;
+        public bool useTotalLevels { get; set; }
+        public int color { get; set; }
+        public bool noAfkPenalty { get; set; }
+        public bool displayGroupName { get; set; }
+        public bool showInList { get; set; } = true;
+        public Dictionary<Skill, float> groupfor { get; set; } = [];
+
+    }
+}
